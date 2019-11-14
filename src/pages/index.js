4b2ac34ef2d3794145da16cd24175287d6a841f7
Jorge Vivas <yorch.vivas@gmail.com>
2019-11-14 09:31:45 -0800

    Initial Web

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi Food Lovers</h1>
+    <p>Welcome.</p>
+    <p>We will have some content here in the future. Don't panic.</p>
+    <p>You can contact us at contact@croppia.com</p>
   </Layout>
 )
 
